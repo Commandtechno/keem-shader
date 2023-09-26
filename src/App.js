4f3e3b9e3e4a2c0d4a4fcf05ec4753bc53f7bee1
Laiqa Mohid <55717512+laimoh@users.noreply.github.com>
2023-09-26 20:16:37 +0100

    glitch and invert periodic w/ eery font

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import React, { useRef, useState, useEffect } from "react";
 import { Canvas, extend } from "@react-three/fiber";
 import { OrbitControls, Effects } from '@react-three/drei';
 import { BloomPass } from "three/examples/jsm/postprocessing/BloomPass";
-import { GlitchPass } from "three/examples/jsm/postprocessing/GlitchPass";
+import learning from './learning.svg'
+import { GlitchPass } from "./GlitchPass";
 import Eyes from './components/Eyes';
 import { Suspense } from "react";
 
 
-extend({ GlitchPass, BloomPass });
+extend({ GlitchPass });
 
 const fonts = [
    'Redaction',
@@ -25,9 +26,6 @@ const App = () => {
    const isMobile = window.innerWidth <= 576; // Adjust the threshold as needed
    // const scaleFactor = isMobile? '150' : '90';
   const [mousePosition, setMousePosition] = useState([0, 0]);
-  const [prevMousePosition, setPrevMousePosition] = useState([0, 0]);
-  const [currentTime, setCurrentTime] = useState(0);
-  
   const [deviceOrientation, setDeviceOrientation] = useState({ alpha: 0, beta: 0, gamma: 0 });
   const [hasPermission, setHasPermission] = useState(false); // Permission state
 
@@ -35,22 +33,23 @@ const App = () => {
   const charRef = useRef(null)
   const [fontIndex, setFontIndex] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setFontIndex((prevIndex) => (prevIndex + 1) % fonts.length);
-    }, 150); // Change fonts every 500 milliseconds
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setFontIndex((prevIndex) => (prevIndex + 1) % fonts.length);
+//     }, 150); // Change fonts every 500 milliseconds
 
-    return () => {
-      clearInterval(interval); // Cleanup the interval when the component unmounts
-    };
-  }, []);
+//     return () => {
+//       clearInterval(interval); // Cleanup the interval when the component unmounts
+//     };
+//   }, []);
 
-  useEffect(() => {
-    const charElements = charRef.current.querySelectorAll('span');
-    charElements.forEach((char, index) => {
-      char.style.fontFamily = fonts[(fontIndex + index) % fonts.length];
-    });
-  }, [fontIndex]);
+//   useEffect(() => {
+//     const charElements = charRef.current.querySelectorAll('span');
+//     charElements.forEach((char, index) => {
+//       char.style.fontFamily = fonts[(fontIndex + index) % fonts.length];
+//     });
+//   }, [fontIndex]);
+
 
   const getPermssion = (e) => {
    if (typeof DeviceOrientationEvent !== "undefined" && typeof DeviceOrientationEvent.requestPermission === "function") {
@@ -88,28 +87,10 @@ const App = () => {
    const { clientX, clientY } = event;
    const mouseX = (clientX / window.innerWidth) * 2 - 1;
    const mouseY = -(clientY / window.innerHeight) * 2 + 1;
- 
-   // // Calculate the time difference since the last mouse move
-   // const newTime = performance.now();
-   // const deltaTime = newTime - currentTime;
- 
-   // // Calculate the change in mouse position
-   // const deltaX = mouseX - prevMousePosition[0];
-   // const deltaY = mouseY - prevMousePosition[1];
- 
-   // // Calculate mouse speed based on change in position and time
-   // const mouseSpeed = Math.sqrt(deltaX ** 2 + deltaY ** 2) / deltaTime;
- 
+
    setMousePosition([mouseX, mouseY]);
-   // setPrevMousePosition([mouseX, mouseY]);
-   // setCurrentTime(newTime);
 
-   //    if (mouseSpeed > 0.018)  {
-
-   //    }
    }};
-
-   
 
 
   return (
@@ -117,23 +98,28 @@ const App = () => {
       <div style={{ width: "100vw", height: "100%" }} onMouseMove={onMouseMove}>
      { !hasPermission && isMobile && 
      <div onClick={getPermssion} className="enter"> <p>ENTER</p></div> }
-            <p ref={charRef} className='diction'>
+            {/* <p ref={charRef} className='diction'>
         {Array.from('Learning...').map((char, index) => (
           <span key={index}>{char}</span>
         ))}
-      </p>
+      </p> */}
+         <div className='diction'>
+         <img src={learning} alt="learning" />
+         <span>.</span>
+         <span>.</span>
+         <span>.</span>
+         </div>
         <main
           ref={mainRef}
           style={{background: '#BABABA'}}>
           <Canvas camera={{ position: [0, 0, 300] }} >
                <Effects>
-                  <bloomPass attach="passes" factor={1}/>
+  
                   <glitchPass attach="passes"/>
                </Effects>
-               {/* <ambientLight/>
-               <pointLight position={[0, 0, 15]} intensity={1} />
-               <pointLight position={[3, -3, 20]} />
-               <fog attach="fog" args={['#000', 2, 1]} /> */}
+               <directionalLight intensity={0.05} />
+                <pointLight intensity={0.2} color="red" />
+                <fog attach="fog" args={['#000', 2, 1.5]} /> *
             <Suspense fallback={null}>     
             {isMobile ? (
                <Eyes mousePosition={null} deviceOrientation={deviceOrientation} />
@@ -141,7 +127,7 @@ const App = () => {
                <Eyes mousePosition={mousePosition} deviceOrientation={null} />
             )}
             </Suspense>
-            <OrbitControls/>
+            {/* <OrbitControls/> */}
           </Canvas>
         </main>
       </div>
--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -5,71 +5,6 @@ import { useGLTF, shaderMaterial } from "@react-three/drei";
 useGLTF.preload("/Eyes_Keem.glb");
 
 
-export const ImageFadeMaterial = shaderMaterial(
-   {
-     uTime: 0
-   },
-   /*glsl */` 
-   varying vec2 vUv;
-     void main() {
-       vUv = uv;
-       gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-     }`,
-   
-     /*glsl */` 
-     #define ss(a,b,t) smoothstep(a,b,t)
-     varying vec2 vUv;
-     uniform float uTime;
-
-     float gyroid (vec3 seed) {
-         return dot(sin(seed), cos(seed.yzx));
-      }
-      
-      float fbm (vec3 seed) {
-            float result = 0.0, a = 0.5;
-            for (int i = 0; i < 5; ++i, a /= 2.0) {
-               seed.z += result * 0.5;
-               result += abs(gyroid(seed / a) * a);
-            }
-            return result;
-      }
-      
-      vec3 getColor(float t) {
-            // Define a color palette here
-            vec3 colors[5];
-            colors[0] = vec3(0.0, 0.5, 0.0); // Blue
-            colors[1] = vec3(0.0, 1.0, 0.0); // Green
-            colors[2] = vec3(1.0, 0.0, 0.0); // Red
-            colors[3] = vec3(1.0, 1.0, 0.0); // Yellow
-            colors[4] = vec3(0.8, 0.2, 0.8); // Purple
-            
-            int numColors = 5;
-            int index = int(floor(t * float(numColors)));
-            return colors[index];
-      }
-
-     void main() {
-      vec2 p = vUv;
-      float d = length(p);
-      p = normalize(p) * log(length(p) * 0.5);
-      p = vec2(atan(p.y, p.x), length(p) * 0.5 + uTime * 0.5);
-      float shades = 6.0;
-      float shape = ss(0.9, 0.5, fbm(vec3(p * 0.5, 0.0)));
-      float shade = ceil(shape * shades) / shades;
-      
-      // Get a color based on the shade value
-      vec3 bgColor = getColor(shade);
-      
-      vec3 color = mix(bgColor, vec3(1.0), ss(2.0, 0.0, d));
-      
-       vec4 finalTexture = vec4(color, 1.0);
-       gl_FragColor = finalTexture;
-
-     }`
- )
- 
- extend({ ImageFadeMaterial })
-
 const Eyes = ({ mousePosition, deviceOrientation }) => {
 
    const isMobile = window.innerWidth <= 576; 
@@ -85,15 +20,15 @@ const Eyes = ({ mousePosition, deviceOrientation }) => {
 
      useFrame(({clock}) => {
 
-      // shaderRef.current.uTime = clock.getElapsedTime();
+      // shaderRef.current.uTime = clock.getElapsedTime() * 0.5;
 
       if (mousePosition) {
-       const targetRotationX = -mousePosition[1] * Math.PI / 8;
-       const targetRotationY = mousePosition[0] * Math.PI / 8;
+       const targetRotationX = -mousePosition[1] * Math.PI / 4;
+       const targetRotationY = mousePosition[0] * Math.PI / 12;
        const clampedRotationY = Math.max(-0.5, Math.min(0.5, targetRotationY));
-       const clampedRotationX = Math.max(-0.2, Math.min(0.5, targetRotationX));
+       const clampedRotationX = Math.max(0, Math.min(0.5, targetRotationX));
    
-       objRef.current.rotation.x += 0.09 * (clampedRotationX - objRef.current.rotation.x);
+       objRef.current.rotation.x += 0.05 * (clampedRotationX - objRef.current.rotation.x);
        objRef.current.rotation.y += 0.09 * (clampedRotationY - objRef.current.rotation.y); 
        
       } else if (deviceOrientation) {
@@ -128,8 +63,6 @@ const Eyes = ({ mousePosition, deviceOrientation }) => {
          material={nodes.Volume_Mesher.material}
          position={objPos}>
             <meshStandardMaterial emissive={"grey"} color={"#5b5b5b"}/>
-            {/* <colorShiftMaterial uColor={"pink"} ref={shaderRef} /> */}
-            {/* <imageFadeMaterial ref={shaderRef} /> */}
       </mesh>
        
      </group>
