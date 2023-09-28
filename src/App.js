41120ca3d07b2b0370d47027b241c06a6cb336a9
Laiqa Mohid <55717512+laimoh@users.noreply.github.com>
2023-09-28 14:02:27 +0100

    frosted glass material added
    commented out glitch effect bcoz of error and no mobile support

--- src/App.js
+++ src/App.js
@@ -2,83 +2,19 @@ import './App.css'
 import React, { useRef, useState, useEffect } from "react";
 import { Canvas, extend } from "@react-three/fiber";
 import { OrbitControls, Effects, Environment } from '@react-three/drei';
-import learning from './learning.svg'
-import { GlitchPass } from "./GlitchPass";
+// import { GlitchPass } from './GlitchPass';
+import {ReactComponent as Learning} from './learning.svg';
 import Eyes from './components/Eyes';
 import { Suspense } from "react";
 
-
-extend({ GlitchPass });
-
-const fonts = [
-   'Redaction',
-   'Ferrum',
-   'FA1',
-   'Nacelle',
-   'Keem',
-   'Seshat',
-   'Times New Roman',
- ];
+// extend({ GlitchPass })
 
 const App = () => {
 
-   const isMobile = window.innerWidth <= 576; // Adjust the threshold as needed
-   // const scaleFactor = isMobile? '150' : '90';
+   const isMobile = window.innerWidth <= 576;
   const [mousePosition, setMousePosition] = useState([0, 0]);
-  const [deviceOrientation, setDeviceOrientation] = useState({ alpha: 0, beta: 0, gamma: 0 });
-  const [hasPermission, setHasPermission] = useState(false); // Permission state
-
+ 
   const mainRef = useRef(null);
-  const charRef = useRef(null)
-  const [fontIndex, setFontIndex] = useState(0);
-
-//   useEffect(() => {
-//     const interval = setInterval(() => {
-//       setFontIndex((prevIndex) => (prevIndex + 1) % fonts.length);
-//     }, 150); // Change fonts every 500 milliseconds
-
-//     return () => {
-//       clearInterval(interval); // Cleanup the interval when the component unmounts
-//     };
-//   }, []);
-
-//   useEffect(() => {
-//     const charElements = charRef.current.querySelectorAll('span');
-//     charElements.forEach((char, index) => {
-//       char.style.fontFamily = fonts[(fontIndex + index) % fonts.length];
-//     });
-//   }, [fontIndex]);
-
-
-  const getPermssion = (e) => {
-   if (typeof DeviceOrientationEvent !== "undefined" && typeof DeviceOrientationEvent.requestPermission === "function") {
-      console.log('DeviceOrientationEvent is defined')
-     DeviceOrientationEvent.requestPermission()
-       .then((permissionState) => {
-         if (permissionState === "granted") {
-           setHasPermission(true);
-         } else {
-           console.error("Permission denied for device orientation.");
-         }
-       })
-       .catch((error) => {
-         console.error("Error requesting device orientation permission:", error);
-       });
-      }
-   }
-
-   useEffect(() => {
-      if (hasPermission) {
-        const handleDeviceOrientation = (event) => {
-         const { alpha, beta, gamma } = event;  
-          setDeviceOrientation({ alpha, beta, gamma });
-        };
-        window.addEventListener("deviceorientation", handleDeviceOrientation);
-        return () => {
-          window.removeEventListener("deviceorientation", handleDeviceOrientation);
-        };
-      }
-    }, [hasPermission]);
 
 
   const onMouseMove = (event) => {
@@ -86,25 +22,16 @@ const App = () => {
    const { clientX, clientY } = event;
    const mouseX = (clientX / window.innerWidth) * 2 - 1;
    const mouseY = -(clientY / window.innerHeight) * 2 + 1;
-
    setMousePosition([mouseX, mouseY]);
-
    }};
-
 
 
   return (
     <>
-      <div style={{ width: "100vw", height: "100%" }} onMouseMove={onMouseMove}>
-     { !hasPermission && isMobile && 
-     <div onClick={getPermssion} className="enter"> <p>ENTER</p></div> }
-            {/* <p ref={charRef} className='diction'>
-        {Array.from('Learning...').map((char, index) => (
-          <span key={index}>{char}</span>
-        ))}
-      </p> */}
+      <div style={{ width: "100vw", height: "100%" }} 
+      onMouseMove={onMouseMove}>
          <div className='diction'>
-         <img src={learning} alt="learning" />
+         <Learning/>
          <span>.</span>
          <span>.</span>
          <span>.</span>
@@ -112,22 +39,16 @@ const App = () => {
         <main
           ref={mainRef}
           style={{background: '#BABABA'}}>
-          <Canvas camera={{ position: [0, 0, 300] }} >
-               <Effects>
-                  <glitchPass attach="passes"/>
-               </Effects>
-               <ambientLight intensity={5} />
-               <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, -5]} castShadow />
-               {/* <Environment preset="city" background blur={2} /> */}
-            <Suspense fallback={null}>     
-            
-            {isMobile ? (
-               <Eyes mousePosition={null} deviceOrientation={deviceOrientation} />
-               ) : (
-               <Eyes mousePosition={mousePosition} deviceOrientation={null} />
-            )}
-            </Suspense>
-            <OrbitControls/>
+          <Canvas camera={{ position: [0, -0.2, 1.3] }} >
+               {/* <Effects>
+                  <glitchPass attachArray="passes"/>
+               </Effects> */}
+               {/* <ambientLight intensity={0.1} /> */}
+               <Environment files="./img/environment.hdr" background blur={0.5} />
+               <Suspense fallback={null}>     
+                  <Eyes mousePosition={mousePosition} deviceOrientation={null} />
+               </Suspense>
+            {/* <OrbitControls/> */}
           </Canvas>
         </main>
       </div>
--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -1,62 +1,36 @@
 import * as THREE from 'three';
  import React, { useRef, useState } from 'react'
- import { useFrame, extend } from "@react-three/fiber";
-import { useGLTF, MeshTransmissionMaterial, shaderMaterial } from "@react-three/drei";
+ import { useFrame, useThree } from "@react-three/fiber";
+import { useGLTF, MeshTransmissionMaterial } from "@react-three/drei";
 useGLTF.preload("/Eyes_Keem.glb");
 
-
-function FrostedGlass({children}) {
-   return (
-      <>
-         <mesh scale={[window.innerWidth, window.innerHeight, 1]} color={'#f0f0f0'} position={[0, 0, -5]}>
-         <circleGeometry args={[1, 1]} />
-         <MeshTransmissionMaterial 
-          samples={8}
-          resolution={512}
-          anisotropy={1}
-          thickness={0.1}
-          roughness={0.2}
-          toneMapped={true}
-          depthWrite={false} // Disable depth writing to render behind other objects
-          transparent // Enable transparency
-          opacity={0.7} // Adjust opacity as needed
-          renderOrder={1}
-         />
-         </mesh>
-        <group>
-          {children}
-        </group>
-      </>
-    )
-}
 
 const Eyes = ({ mousePosition, deviceOrientation }) => {
 
    const isMobile = window.innerWidth <= 576; 
    const objRef = useRef();
    const shaderRef = useRef();
-   const objScale = isMobile ? 0.58 : 1;
-   const objPos = isMobile ? [30, 0, 0] : [45.463, -29.926, 22.715]
+   const objScale = isMobile ? 0.58 : 0.01;
+   const objPos = isMobile ? [30, 0, 0] : [0.5, 0, -1.5]
    const { nodes, materials } = useGLTF("/Eyes_Keem.glb");
   
      const phoneAngle = 90; // Set the initial beta value you want
       const sensitivityY = 0.03; 
       const sensitivityX = 0.04; 
+      const viewport = useThree(state => state.viewport)
 
      useFrame(({clock}) => {
-
-      // shaderRef.current.uTime = clock.getElapsedTime() * 0.5;
-
       if (mousePosition) {
        const targetRotationX = -mousePosition[1] * Math.PI / 4;
        const targetRotationY = mousePosition[0] * Math.PI / 12;
-       const clampedRotationY = Math.max(-0.5, Math.min(0.5, targetRotationY));
-       const clampedRotationX = Math.max(0, Math.min(0.5, targetRotationX));
+       const clampedRotationY = Math.max(-1, Math.min(0.5, targetRotationY));
+       const clampedRotationX = Math.max(0.1, Math.min(1, targetRotationX));
    
        objRef.current.rotation.x += 0.05 * (clampedRotationX - objRef.current.rotation.x);
        objRef.current.rotation.y += 0.09 * (clampedRotationY - objRef.current.rotation.y); 
        
       } else if (deviceOrientation) {
+
          // const alpha = deviceOrientation.alpha;
          const beta = deviceOrientation.beta || 0;
          const gamma = deviceOrientation.gamma || 0;
@@ -77,7 +51,12 @@ const Eyes = ({ mousePosition, deviceOrientation }) => {
      });
      return (
       <>
-      <FrostedGlass>
+      <group>
+         <mesh position={[0.5, 0, 0.5]} scale={[viewport.width, viewport.height, 1]}>
+            <planeGeometry args={[1, 1, 1]}/>
+            <MeshTransmissionMaterial samples={4} resolution={32} anisotropy={1} thickness={0.1} roughness={0.3} toneMapped={true} />
+         </mesh>
+      </group>
          <group dispose={null} >
             <mesh ref={objRef}
                scale={objScale}
@@ -86,10 +65,11 @@ const Eyes = ({ mousePosition, deviceOrientation }) => {
                geometry={nodes.Volume_Mesher.geometry}
                material={nodes.Volume_Mesher.material}
                position={objPos}>
-               <meshStandardMaterial emissive={"grey"} color={"#5b5b5b"}/>
+               <meshStandardMaterial emissive={"grey"} color={"#1f1e1e"}/>
+               {/* <shaderMaterial ref={shaderRef} attach="material" args={[ShaderMaterial]} /> */}
             </mesh>
          </group>
-      </FrostedGlass>
+
       </>
       
      );
