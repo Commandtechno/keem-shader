c6729658cac87bb21f324355bb3cb113fd133769
Laiqa Mohid <55717512+laimoh@users.noreply.github.com>
2023-10-19 23:37:37 +0100

    reduced the camera zoom out

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ const App = () => {
          </div>
         <main
           style={{backgroundColor: '#fAfffA'}}>
-          <Canvas camera={{ position: [0, 0, 1000] }} >
+          <Canvas camera={{ position: [0, 0, 300] }} >
                <Effects>
                   <glitchPass attach="passes"/>
                </Effects>
