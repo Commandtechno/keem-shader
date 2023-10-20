ee9522a3f321600bfcca81deecdbc724c4f67406
Laiqa Mohid <55717512+laimoh@users.noreply.github.com>
2023-10-20 11:16:40 +0100

    added a loader

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ const App = () => {
    const [mousePosition, setMousePosition] = useState([0, 0]);
    const [deviceOrientation, setDeviceOrientation] = useState({ alpha: 0, beta: 0, gamma: 0 });
    const [hasPermission, setHasPermission] = useState(false); // Permission state
-   const [enter, isEnter] = useState(false)
+   const [enter, isEnter] = useState(false);
+   const [loader, setLoader] = useState(true);
 
   const getPermssion = (e) => {
    if (typeof DeviceOrientationEvent !== "undefined" && typeof DeviceOrientationEvent.requestPermission === "function") {
@@ -58,6 +59,24 @@ const App = () => {
       setMousePosition([mouseX, mouseY]);
    }};
 
+     // This will run one time after the component mounts
+  useEffect(() => {
+   // callback function to call when event triggers
+   const onPageLoad = () => {
+     setLoader(false)
+   };
+
+   // Check if the page has already loaded
+   if (document.readyState === 'complete') {
+     onPageLoad();
+   } else {
+     window.addEventListener('load', onPageLoad, false);
+     // Remove the event listener when component unmounts
+     return () => window.removeEventListener('load', onPageLoad);
+   }
+ }, []);
+
+   if (loader) return <div>Loading..</div>
   return (
     <>
       <div style={{ width: "100vw", height: "100%" }} onMouseMove={onMouseMove}>
